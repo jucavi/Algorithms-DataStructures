314da3bea8e3abd0cdd6e9bfc55b61298853f310
Jucavi <jcarvil@gmail.com>
2022-05-23 11:58:29 +0200

    add singly linked list

---
+++ singly-linked-list/singlyLikedList.js
@@ -0,0 +1,135 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  push(value) {
+    let node = new Node(value);
+    if (this.head) {
+      this.tail.next = node;
+      this.tail = node;
+    } else {
+      this.head = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+
+    if (this.head !== this.tail) {
+      while (current.next) {
+        newTail = current;
+        current = current.next;
+      }
+      this.tail = newTail;
+      this.tail.next = null;
+      this.length--;
+    } else {
+      this.length = 0;
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    this.head = current.next;
+    if (current === this.tail) {
+      this.tail = null;
+    }
+    this.length--;
+    return current;
+  }
+
+  unshift(value) {
+    let newNode = new Node(value);
+    let current = this.head;
+
+    if (current) {
+      newNode.next = current;
+    } else {
+      this.tail = newNode;
+    }
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count = 0;
+    let current = this.head;
+
+    while (count !== index) {
+      current = current.next;
+      count++;
+    }
+    return current;
+  }
+
+  set(index, value) {
+    let node = this.get(index);
+    if (node) {
+      node.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index > this.length) return false;
+    if (index === 0) return !!this.unshift(value);
+    if (index === this.length) !!this.push(value);
+
+    let insertNode = new Node(value);
+    let node = this.get(index - 1);
+    let nextNode = node.next;
+    insertNode.next = nextNode;
+    node.next = insertNode;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return false;
+    if (index === 0) return !!this.shift();
+    if (index === this.length - 1) return !!this.pop();
+
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    let next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node
+      node = next
+    }
+    return this
+  }
+}
+
