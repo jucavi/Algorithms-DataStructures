143fe398e1b5eafd18e886bb67158c6d0b58900a
Jucavi <jcarvil@gmail.com>
2022-05-23 12:27:49 +0200

    fix: reverse method

--- singly-linked-list/singlyLikedList.js
+++ singly-linked-list/singlyLikedList.js
@@ -9,6 +9,7 @@ class SinglyLinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    this.length = 0;
   }
 
   push(value) {
@@ -120,6 +121,7 @@ class SinglyLinkedList {
   reverse() {
     let node = this.head;
     this.head = this.tail;
+    this.tail = node
     let next;
     let prev = null;
 
